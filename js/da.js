b80cbf75ed71adf41f04f9c55cd12ef16d77dff8
jtjocowi <jtjo@cowi.com>
2026-06-16 14:24:15 +0200

    Update Danish language strings for consistency and accuracy

--- js/da.js
+++ js/da.js
@@ -1,12 +1,12 @@
 var language_strings = language_strings || {};
 language_strings['da'] = {
-    "step_edit_proposed_plan": "Rediger",
-    "step_validate_proposed_plan": "Valider",
-    "step_proposed_plan": "Indrapporter",
-    "step_edit_approved_plan": "Rediger",
-    "step_validate_approved_plan": "Valider",
-    "step_approved_plan": "Indrapporter",
-    "plandata_proposed_plan": "PLANFORSLAG Indrapporteret",
-    "plandata_approved_plan": "VEDTAGET PLAN Indrapporteret",
+    "step_edit_proposed_plan": "Redigér",
+    "step_validate_proposed_plan": "Validér",
+    "step_proposed_plan": "Indberet",
+    "step_edit_approved_plan": "Redigér",
+    "step_validate_approved_plan": "Validér",
+    "step_approved_plan": "Indberet",
+    "plandata_proposed_plan": "FORSLAG Indberettet",
+    "plandata_approved_plan": "VEDTAGET Indberettet",
     "plandata_logo": "Plandata.dk"
 };
